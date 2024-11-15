ff4d40a93dc28d43ba917869b66a57374e3967d9
Ahmed Abd El Hady <83279919+Ahmed19mohamed12@users.noreply.github.com>
2024-11-15 23:40:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const client = new Client({
 });
 
 client.on('qr', (qr) => {
-    qrcode.generate(qr, {small: true});
+    qrcode.generate(qr, {small: false});
     console.log('QR RECEIVED', qr);
 });
 
