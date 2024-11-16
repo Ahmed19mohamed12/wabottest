98fba2aca6bf0a615bdaec0412cdec822923a3f8
Ahmed Abd El Hady <83279919+Ahmed19mohamed12@users.noreply.github.com>
2024-11-16 23:21:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.post("/send",(req,res)=>{
         authStrategy: new LocalAuth() // Use LocalAuth for session management
     });
     
-    client.on('qr', (qr) => {
+    client.on('qr', async (qr) => {
         qrcode.generate(qr, {small: true});
         const qrCodeImage = await QRCode.toDataURL(qr);
         res.send(`<img src="${qrCodeImage}" alt="QR Code"/>`);
