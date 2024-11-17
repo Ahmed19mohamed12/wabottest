3a004caa07b2dc2e6ab6f6d88769c75357fba638
Ahmed Abd El Hady <83279919+Ahmed19mohamed12@users.noreply.github.com>
2024-11-17 21:26:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,54 +1,37 @@
 const { Client, LocalAuth ,MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
-const express = require('express');
-const QRCode = require('qrcode');
-const app = express();
 const fs = require('fs');
 const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
-app.use(express.json());
-app.post("/send",(req,res)=>{
-    const client = new Client({
-        puppeteer: {
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        },
-        // authStrategy: new LocalAuth() // Use LocalAuth for session management
-    });
+const client = new Client({
+    authStrategy: new LocalAuth() // Use LocalAuth for session management
+});
 
-    client.on('qr', async (qr) => {
-        qrcode.generate(qr, {small: true});
-        const qrCodeImage = await QRCode.toDataURL(qr);
-        res.send(`<img src="${qrCodeImage}" alt="QR Code"/>`);
-        console.log(qr);
-    });
-    
-    client.on('ready',  async () => {
-        console.log('Client is ready!');
-    
-        // Read the list of numbers from a JSON file
-        const filename = req.body['filename'];
-        const message = req.body['message'];
-        let parents = fs.readFileSync(filename+'.json');
-        parents = JSON.parse(parents);
-    
-        // Loop through the numbers and send messages
-        for (let i = 0; i < parents.length; i++) {
-            await delay(5000);
-            const media = MessageMedia.fromFilePath('1.jpg');
-            let number = parents[i].number + '@c.us'; // Format the number
-            client.sendMessage(number,media,{caption:message})
+client.on('qr', (qr) => {
+    qrcode.generate(qr, {small: true});
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('ready',  async () => {
+    console.log('Client is ready!');
+
+    // Read the list of numbers from a JSON file
+    let parents = fs.readFileSync('parents.json');
+    parents = JSON.parse(parents);
+
+    // Loop through the numbers and send messages
+    for (let i = 0; i < parents.length; i++) {
+        const media = MessageMedia.fromFilePath('1.jpg');
+        let number = parents[i].number + '@c.us'; // Format the number
+        client.sendMessage(number,media,{caption: 'أهلا وسهلا\n ♥♥ يا احمد'})
             .then(response => {
                 console.log(`Message sent to ${number}:`, response);
             })
             .catch(err => {
                 console.error(`Failed to send message to ${number}:`, err);
             });
-        }
-    });
-    client.initialize();
-    
-    // res.status(200).send("Success");
-})
-app.listen(3000, () => {
-    
-    console.log("Started on port 3000");
+        await delay(5000);
+        
+    }
 });
+
+client.initialize();
